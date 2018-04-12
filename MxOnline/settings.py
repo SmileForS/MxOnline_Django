7363b069277a630fd60c9507a140a3e3d7e6b41b
LL <cpyfluoxiaohui@163.com>
2018-04-12 20:46:14 +0800

    静态文件配置和主页设置

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -137,3 +137,4 @@ USE_TZ = False  # 这个改成False表示是本地时间，不是UTC的时间
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
