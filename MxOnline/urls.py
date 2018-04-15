57200222433395ebff1dabcb07eb6b33c6074b22
LL <cpyfluoxiaohui@163.com>
2018-04-15 18:16:25 +0800

    base模板基本实现，根据课程机构首页来设置模板

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -16,13 +16,17 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.views.generic import TemplateView
 from users.views import LoginView,RegisterView,ActiveUserView,ForgetPwdView,ResetView,ModifyView
+from organization.views import OrgListView
 import xadmin
 import captcha
 
 urlpatterns = [
+    # 后台管理页面
     url(r'^xadmin/', xadmin.site.urls),
+    # 首页
     url(r'^$',TemplateView.as_view(template_name='index.html'),name='index'),
     # url(r"^login$",TemplateView.as_view(template_name='login.html'),name='login'),
+    # 登录和注册页面
     url(r'^login$',LoginView.as_view(),name='login'),
     url(r'^register$',RegisterView.as_view(),name='register'),
     url(r'^captcha/', include('captcha.urls')),
@@ -30,4 +34,6 @@ urlpatterns = [
     url(r'^forget$',ForgetPwdView.as_view(),name='forgetpwd'),
     url(r'^reset/(?P<reset_code>.*)$',ResetView.as_view(),name='reset'),
     url(r'^modifypwd$',ModifyView.as_view(),name='modifypwd'),
+    # 课程机构首页
+    url(r'^org_list$',OrgListView.as_view(),name='org_list'),
 ]
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+
+class OrgListView(View):
+    """课程机构视图"""
+    def get(self,request):
+        """显示页面"""
+        return render(request,'org-list.html')
